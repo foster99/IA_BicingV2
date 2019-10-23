739da7c114307c3407dba8d60892250a8ffaaf35
Bartomeu Perelló <byidarkgenesis@gmail.com>
2019-10-23 11:46:00 +0200

    Pair owo

--- bicing-aima/src/model/Pair.java
+++ bicing-aima/src/model/Pair.java
@@ -1,4 +1,15 @@
 package model;
 
 public class Pair {
+    public final int first;
+    public int second;
+
+    public Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public Pair clone() {
+        return new Pair(first, second);
+    }
 }
