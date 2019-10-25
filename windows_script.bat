2a69de0b055df551aa90b429f6c23a23ee4dc84f
Edgar Perez <edgarperezblanco@gmail.com>
2019-10-25 16:23:13 +0200

    JAR + Windows Script

---
+++ windows_script.bat
@@ -0,0 +1,42 @@
+ECHO "TESTING IA_BICING v2"
+> output.txt (
+    ECHO "Entrada: nest = 25 | nbic = 1250 | nfur = 5 | dem = 0 | seed = 1234 | init_sol = 0 | algorithm = 0"
+    java -jar .\out\artifacts\bicing_aima_jar\bicing-aima.jar 25 1250 5 0 1234 0 0
+    ECHO.
+    ECHO "Entrada: nest = 50 | nbic = 1250 | nfur = 5 | dem = 0 | seed = 1234 | init_sol = 0 | algorithm = 0"
+    java -jar .\out\artifacts\bicing_aima_jar\bicing-aima.jar 50 2500 10 0 1234 0 0
+    ECHO.
+    ECHO "Entrada: nest = 75 | nbic = 1250 | nfur = 5 | dem = 0 | seed = 1234 | init_sol = 0 | algorithm = 0"
+    java -jar .\out\artifacts\bicing_aima_jar\bicing-aima.jar 75 3750 5 15 1234 0 0
+    ECHO.
+    ECHO "Entrada: nest = 100 | nbic = 1250 | nfur = 5 | dem = 0 | seed = 1234 | init_sol = 0 | algorithm = 0"
+    java -jar .\out\artifacts\bicing_aima_jar\bicing-aima.jar 100 5000 20 0 1234 0 0
+    ECHO.
+    ECHO "Entrada: nest = 125 | nbic = 1250 | nfur = 5 | dem = 0 | seed = 1234 | init_sol = 0 | algorithm = 0"
+    java -jar .\out\artifacts\bicing_aima_jar\bicing-aima.jar 125 6250 25 0 1234 0 0
+    ECHO.
+    ECHO "Entrada: nest = 150 | nbic = 1250 | nfur = 5 | dem = 0 | seed = 1234 | init_sol = 0 | algorithm = 0"
+    java -jar .\out\artifacts\bicing_aima_jar\bicing-aima.jar 150 7500 30 0 1234 0 0
+    ECHO.
+    ECHO "Entrada: nest = 175 | nbic = 1250 | nfur = 5 | dem = 0 | seed = 1234 | init_sol = 0 | algorithm = 0"
+    java -jar .\out\artifacts\bicing_aima_jar\bicing-aima.jar 175 8750 35 0 1234 0 0
+    ECHO.
+    ECHO "Entrada: nest = 200 | nbic = 1250 | nfur = 5 | dem = 0 | seed = 1234 | init_sol = 0 | algorithm = 0"
+    java -jar .\out\artifacts\bicing_aima_jar\bicing-aima.jar 200 10000 40 0 1234 0 0
+    ECHO.
+    ECHO "Entrada: nest = 300 | nbic = 1250 | nfur = 5 | dem = 0 | seed = 1234 | init_sol = 0 | algorithm = 0"
+    java -jar .\out\artifacts\bicing_aima_jar\bicing-aima.jar 225 11250 45 0 1234 0 0
+    ECHO.
+    ECHO "Entrada: nest = 400 | nbic = 1250 | nfur = 5 | dem = 0 | seed = 1234 | init_sol = 0 | algorithm = 0"
+    java -jar .\out\artifacts\bicing_aima_jar\bicing-aima.jar 250 12500 50 0 1234 0 0
+    ECHO.
+    ECHO "Entrada: nest = 500 | nbic = 1250 | nfur = 5 | dem = 0 | seed = 1234 | init_sol = 0 | algorithm = 0"
+    java -jar .\out\artifacts\bicing_aima_jar\bicing-aima.jar 500 25000 100 0 1234 0 0
+    ECHO.
+    ECHO "Entrada: nest = 750 | nbic = 1250 | nfur = 5 | dem = 0 | seed = 1234 | init_sol = 0 | algorithm = 0"
+    java -jar .\out\artifacts\bicing_aima_jar\bicing-aima.jar 750 37500 150 0 1234 0 0
+    ECHO.
+    ECHO "Entrada: nest = 750 | nbic = 1250 | nfur = 5 | dem = 0 | seed = 1234 | init_sol = 0 | algorithm = 0"
+    java -jar .\out\artifacts\bicing_aima_jar\bicing-aima.jar 1000 50000 200 0 1234 0 0
+)
+
